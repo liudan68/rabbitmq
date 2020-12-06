4cb14c988aa7fa4998bb614663dc4adb0b091f4b
agileluo <marlon.luo@qq.com>
2020-12-06 19:14:28 +0800

    first commit

---
+++ src/main/java/RabbitMqHelloApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author dandan
+ * @create 2020/12/4 13:19
+ */
+public class RabbitMqHelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqHelloApplication.class, args);
+    }
+}
